957bd5aadafed52d534d899dc384db4f2a81c3b6
Forson Gao <fzxgao@gmail.com>
2025-08-21 12:28:53 +0100

    updated initial window size

--- fomo/cli.py
+++ fomo/cli.py
@@ -28,6 +28,6 @@ def main(argv=None):
         scroll_mult=args.scroll_mult,
         scroll_max_streak=args.scroll_max_streak,
     )
-    w.resize(1200, 800)
+    w.resize(1200, 945)
     w.show()
     sys.exit(app.exec_())

3b680ce29400edf11bc2fd81d08847f3778c07d6
Forson Gao <fzxgao@gmail.com>
2025-08-22 20:39:52 +0100

    bug fixes

--- fomo/widgets/picking_panel.py
+++ fomo/widgets/picking_panel.py
@@ -1,5 +1,8 @@
 from PyQt5 import QtCore, QtWidgets
 
+def _disable_scroll(widget):
+    """Prevent mouse wheel events from altering widget value."""
+    widget.wheelEvent = lambda event: event.ignore()
 
 class ModelListWidget(QtWidgets.QListWidget):
     """List widget that allows deleting models with Del key and activating on double click."""
@@ -51,18 +54,23 @@ class PickingSidePanel(QtWidgets.QSplitter):
         form = QtWidgets.QFormLayout()
         self.smooth_radius = QtWidgets.QDoubleSpinBox()
         self.smooth_radius.setValue(5)
+        _disable_scroll(self.smooth_radius)
         form.addRow("Smoothing radius (in pixels)", self.smooth_radius)
         self.smooth_interval = QtWidgets.QDoubleSpinBox()
         self.smooth_interval.setValue(2)
+        _disable_scroll(self.smooth_interval)
         form.addRow("Interval in backbone for smoothing (in pixels)", self.smooth_interval)
         self.subunits_dz = QtWidgets.QDoubleSpinBox()
         self.subunits_dz.setValue(7)
+        _disable_scroll(self.subunits_dz)
         form.addRow("Subunits dz (in pixels, 1/3 to 1/2 of the Helical pitch, if known)", self.subunits_dz)
         self.subunits_dphi = QtWidgets.QDoubleSpinBox()
         self.subunits_dphi.setValue(20)
+        _disable_scroll(self.subunits_dphi)
         form.addRow("Subunits dphi (in degrees, 1/3 to 1/2 of the Helical twist, if known)", self.subunits_dphi)
         self.box_size = QtWidgets.QDoubleSpinBox()
         self.box_size.setValue(40)
+        _disable_scroll(self.box_size)
         form.addRow("Box size (in pixels)", self.box_size)
         v.addLayout(form)
         self.addWidget(params_widget)
--- fomo/widgets/refinement_panel.py
+++ fomo/widgets/refinement_panel.py
@@ -1,5 +1,22 @@
 from PyQt5 import QtCore, QtWidgets
 
+def _disable_scroll(widget):
+    """Prevent mouse wheel events from altering widget value."""
+    widget.wheelEvent = lambda event: event.ignore()
+
+
+def _link_slider_wheel(label: QtWidgets.QLabel, slider: QtWidgets.QSlider):
+    """Allow scrolling over ``label`` to move ``slider`` one step."""
+    def wheel_event(event):
+        delta = event.angleDelta().y()
+        if delta > 0:
+            slider.setValue(min(slider.maximum(), slider.value() + 1))
+        elif delta < 0:
+            slider.setValue(max(slider.minimum(), slider.value() - 1))
+        event.accept()
+
+    label.wheelEvent = wheel_event
+
 
 class RefinementSidePanel(QtWidgets.QSplitter):
     """Side panel shown in normal mode with refinement tools."""
@@ -29,6 +46,7 @@ class RefinementSidePanel(QtWidgets.QSplitter):
             slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
             self.initial_avg_sliders.append(slider)
             vb.addWidget(slider)
+            _link_slider_wheel(label, slider)
             hl.addLayout(vb)
         v.addLayout(hl)
         btns = QtWidgets.QHBoxLayout()
@@ -50,47 +68,92 @@ class RefinementSidePanel(QtWidgets.QSplitter):
         v.addWidget(QtWidgets.QLabel("Numerical parameters"))
         form = QtWidgets.QFormLayout()
         # 1 Iterations
-        self.iterations = QtWidgets.QSpinBox(); self.iterations.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.iterations.setValue(4)
+        self.iterations = QtWidgets.QSpinBox()
+        self.iterations.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.iterations.setValue(4)
+        _disable_scroll(self.iterations)
         form.addRow("Iterations", self.iterations)
         # 2 References
-        self.references = QtWidgets.QSpinBox(); self.references.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.references.setValue(1)
+        self.references = QtWidgets.QSpinBox()
+        self.references.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.references.setValue(1)
+        _disable_scroll(self.references)
         form.addRow("References", self.references)
         # 3 Cone Aperture
-        self.cone_aperture = QtWidgets.QSpinBox(); self.cone_aperture.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.cone_aperture.setValue(45)
+        self.cone_aperture = QtWidgets.QSpinBox()
+        self.cone_aperture.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.cone_aperture.setValue(45)
+        _disable_scroll(self.cone_aperture)
         form.addRow("Cone Aperture", self.cone_aperture)
         # 4 Cone Sampling
-        self.cone_sampling = QtWidgets.QSpinBox(); self.cone_sampling.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.cone_sampling.setValue(15)
+        self.cone_sampling = QtWidgets.QSpinBox()
+        self.cone_sampling.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.cone_sampling.setValue(15)
+        _disable_scroll(self.cone_sampling)
         form.addRow("Cone Sampling", self.cone_sampling)
         # 5 Azimuth Rotation Range
-        self.az_rot_range = QtWidgets.QSpinBox(); self.az_rot_range.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.az_rot_range.setValue(60)
+        self.az_rot_range = QtWidgets.QSpinBox()
+        self.az_rot_range.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.az_rot_range.setValue(60)
+        _disable_scroll(self.az_rot_range)
         form.addRow("Azimuth Rotation Range", self.az_rot_range)
         # 6 Azimuth Rotation Sampling
-        self.az_rot_sampling = QtWidgets.QSpinBox(); self.az_rot_sampling.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.az_rot_sampling.setValue(15)
+        self.az_rot_sampling = QtWidgets.QSpinBox()
+        self.az_rot_sampling.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.az_rot_sampling.setValue(15)
+        _disable_scroll(self.az_rot_sampling)
         form.addRow("Azimuth Rotation Sampling", self.az_rot_sampling)
         # 7 Refine
-        self.refine = QtWidgets.QSpinBox(); self.refine.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.refine.setValue(2)
+        self.refine = QtWidgets.QSpinBox()
+        self.refine.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.refine.setValue(2)
+        _disable_scroll(self.refine)
         form.addRow("Refine", self.refine)
         # 8 Refine Factor
-        self.refine_factor = QtWidgets.QSpinBox(); self.refine_factor.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.refine_factor.setValue(2)
+        self.refine_factor = QtWidgets.QSpinBox()
+        self.refine_factor.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.refine_factor.setValue(2)
+        _disable_scroll(self.refine_factor)
         form.addRow("Refine Factor", self.refine_factor)
         # 9 High Pass
-        self.high_pass = QtWidgets.QSpinBox(); self.high_pass.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.high_pass.setValue(1)
+        self.high_pass = QtWidgets.QSpinBox()
+        self.high_pass.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.high_pass.setValue(1)
+        _disable_scroll(self.high_pass)
         form.addRow("High Pass", self.high_pass)
         #10 Low Pass
-        self.low_pass = QtWidgets.QSpinBox(); self.low_pass.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.low_pass.setValue(12)
+        self.low_pass = QtWidgets.QSpinBox()
+        self.low_pass.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.low_pass.setValue(12)
+        _disable_scroll(self.low_pass)
         form.addRow("Low Pass", self.low_pass)
         #11 Symmetry
         self.symmetry = QtWidgets.QLineEdit("C1")
         form.addRow("Symmetry", self.symmetry)
         #12 Particle Dimensions
-        self.particle_dims = QtWidgets.QSpinBox(); self.particle_dims.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.particle_dims.setValue(40)
+        self.particle_dims = QtWidgets.QSpinBox()
+        self.particle_dims.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.particle_dims.setValue(40)
+        _disable_scroll(self.particle_dims)
         form.addRow("Particle Dimensions", self.particle_dims)
         #13 Shift limits
         shift_widget = QtWidgets.QWidget()
         sh = QtWidgets.QHBoxLayout(shift_widget)
-        self.shift_x = QtWidgets.QSpinBox(); self.shift_x.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.shift_x.setValue(4); self.shift_x.setMinimumWidth(30)
-        self.shift_y = QtWidgets.QSpinBox(); self.shift_y.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.shift_y.setValue(4); self.shift_y.setMinimumWidth(30)
-        self.shift_z = QtWidgets.QSpinBox(); self.shift_z.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.shift_z.setValue(12); self.shift_z.setMinimumWidth(30)
+        self.shift_x = QtWidgets.QSpinBox()
+        self.shift_x.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.shift_x.setValue(4)
+        self.shift_x.setMinimumWidth(30)
+        _disable_scroll(self.shift_x)
+        self.shift_y = QtWidgets.QSpinBox()
+        self.shift_y.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.shift_y.setValue(4)
+        self.shift_y.setMinimumWidth(30)
+        _disable_scroll(self.shift_y)
+        self.shift_z = QtWidgets.QSpinBox()
+        self.shift_z.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.shift_z.setValue(12)
+        self.shift_z.setMinimumWidth(30)
+        _disable_scroll(self.shift_z)
         sh.addWidget(self.shift_x); sh.addWidget(self.shift_y); sh.addWidget(self.shift_z)
         form.addRow("Shift limits", shift_widget)
         #14 Shift limiting way
@@ -102,15 +165,25 @@ class RefinementSidePanel(QtWidgets.QSplitter):
             "From the estimation provided for the first iteration, with static origin",
         ])
         self.shift_way.setCurrentText("From the center of the particle cube")
+        _disable_scroll(self.shift_way)
         form.addRow("Shift limiting way", self.shift_way)
         #15 Separation in Tomogram
-        self.separation = QtWidgets.QSpinBox(); self.separation.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.separation.setValue(20)
+        self.separation = QtWidgets.QSpinBox()
+        self.separation.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.separation.setValue(20)
+        _disable_scroll(self.separation)
         form.addRow("Separation in Tomogram", self.separation)
         #16 Basic MRA
-        self.basic_mra = QtWidgets.QSpinBox(); self.basic_mra.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.basic_mra.setValue(0)
+        self.basic_mra = QtWidgets.QSpinBox()
+        self.basic_mra.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.basic_mra.setValue(0)
+        _disable_scroll(self.basic_mra)
         form.addRow("Basic MRA", self.basic_mra)
         #17 Threshold parameter
-        self.threshold_param = QtWidgets.QDoubleSpinBox(); self.threshold_param.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons); self.threshold_param.setValue(0.2)
+        self.threshold_param = QtWidgets.QDoubleSpinBox()
+        self.threshold_param.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.threshold_param.setValue(0.2)
+        _disable_scroll(self.threshold_param)
         form.addRow("Threshold parameter", self.threshold_param)
         #18 Threshold Mode
         self.threshold_mode = QtWidgets.QComboBox()
@@ -123,6 +196,7 @@ class RefinementSidePanel(QtWidgets.QSplitter):
             "Threshold ranges between 0 and 1  and sets the fraction of particles.",
         ])
         self.threshold_mode.setCurrentText("no thresholding policy")
+        _disable_scroll(self.threshold_mode)
         form.addRow("Threshold Mode", self.threshold_mode)
         #19 Exclusion Mode
         self.exclusion_mode = QtWidgets.QComboBox()
@@ -131,6 +205,7 @@ class RefinementSidePanel(QtWidgets.QSplitter):
             "Exclusion from averaging and alignment",
         ])
         self.exclusion_mode.setCurrentText("No exclusion from averaging and alignment")
+        _disable_scroll(self.exclusion_mode)
         form.addRow("Exclusion Mode", self.exclusion_mode)
         v.addLayout(form)
         self.addWidget(widget)
@@ -142,6 +217,7 @@ class RefinementSidePanel(QtWidgets.QSplitter):
         v.addWidget(QtWidgets.QLabel("Live refinement"))
         hl = QtWidgets.QHBoxLayout()
         self.refined_views = []
+        self.refined_sliders = []
         for axis in ("X", "Y", "Z"):
             vb = QtWidgets.QVBoxLayout()
             label = QtWidgets.QLabel(f"{axis}")
@@ -151,7 +227,9 @@ class RefinementSidePanel(QtWidgets.QSplitter):
             self.refined_views.append(label)
             vb.addWidget(label)
             slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+            self.refined_sliders.append(slider)
             vb.addWidget(slider)
+            _link_slider_wheel(label, slider)
             hl.addLayout(vb)
         v.addLayout(hl)
         btns = QtWidgets.QVBoxLayout()
